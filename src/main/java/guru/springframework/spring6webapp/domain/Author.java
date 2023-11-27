0315bc02ec766695443597b8f42545311ed065eb
mvmcgm13 <mvmcgm@gmail.com>
2023-11-27 16:54:07 +0000

    Bootstrap initial data structure

--- src/main/java/guru/springframework/spring6webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Author.java
@@ -3,6 +3,7 @@ package guru.springframework.spring6webapp.domain;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -16,7 +17,7 @@ public class Author {
     private String lastName;
 
     @ManyToMany(mappedBy = "authors")
-    private Set<Book> books;
+    private Set<Book> books = new HashSet<>();
 
     @Override
     public boolean equals(Object o) {
